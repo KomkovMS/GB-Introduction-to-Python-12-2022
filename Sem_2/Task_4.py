a3d192f8179e3cc09b388f65635a56182ad6022c
KomkovMS <maxkoma05@gmail.com>
2022-12-18 11:43:19 +0700

    Различные варианты решения  задачи 4 - Напишите программу, которая будет принимать на вход дробь и показывать первую цифру дробной части числа

--- Sem_2/Task_4.py
+++ Sem_2/Task_4.py
@@ -9,6 +9,8 @@
 
 # мое решение:
 
+# Вариант 1
+
 import math
 
 number = float(input('Enter a number: '))
@@ -19,3 +21,41 @@ if number * 10 % 10 > 0:
 elif result == 0:
     result = 'нет'
 print(f'{number} -> {result}')
+
+# вариант 2
+number = float(input('Enter a number: '))
+result = 0
+if number * 10 % 10 > 0:
+    result = int(number * 10 % 10)
+elif result == 0:
+    result = 'нет'
+print(f'{number} -> {result}')
+
+# Вариент 3
+number = float(input('Enter a number: '))
+result = 0
+if number % 1 != 0:  # тоже самое что: number != int(number)
+    result = int(number * 10 % 10)
+else:
+    result = 'нет'
+print(f'{number} -> {result}')
+
+# вариант 4 (решение другого ученика на семинаре)
+number = float(input('Enter a number: '))
+result = 0
+if number - int(number) == 0:
+    result = 'нет'
+else:
+    result = int((number - int(number)) * 10)
+
+print(f'{number} -> {result}')
+
+# вариант 5 (решение преподавателя)
+num = input('Enter a number: ')
+result = num.split('.')
+if len(result) > 1:
+    result = result[1][0]
+else:
+    result = 'no'
+
+print(f'{num} -> {result}')
