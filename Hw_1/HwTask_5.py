a38bb5ed0235d86218393d3414ae066379cc55f0
KomkovMS <maxkoma05@gmail.com>
2022-12-20 20:00:45 +0700

    разбор решения ДЗ-1 задача 5 на семинаре от преподавателя

--- Hw_1/HwTask_5.py
+++ Hw_1/HwTask_5.py
@@ -6,6 +6,7 @@
 # A (3,6); B (2,1) -> 5,09
 # A (7,-5); B (1,-1) -> 7,21
 
+import math
 my_list = []
 size = 4
 msg = 'Введиите координаты: '
@@ -29,3 +30,14 @@ result = (((my_list[1] - my_list[0]) * (my_list[1] - my_list[0])) +
           ((my_list[3] - my_list[2])) * (my_list[3] - my_list[2])) ** 0.5
 
 print(f'-> {round(result, 2)}')
+
+
+# разбор решения на семинаре от преподавателя
+
+first = input('Введите координаты точки А через пробел').split()
+second = input('Введите координаты точки B через пробел').split()
+
+distance = (float(first[0]) - float(second[0])) ** 2 + \
+    (float(first[1])) - float(second[1]) ** 2
+
+print(f'расстояние между точками равно {(round(math.sqrt(distance)), 2)}')
