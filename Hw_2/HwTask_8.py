c052fd6dde2c327d0f3c445ca6916d12b23990a0
KomkovMS <maxkoma05@gmail.com>
2022-12-26 20:22:42 +0700

    Разбор ДЗ-2 задача 3 - Реализуйте алгоритм перемешивания списка.

--- Hw_2/HwTask_8.py
+++ Hw_2/HwTask_8.py
@@ -16,3 +16,53 @@ for i in range(len(my_list)):
     i = random.randrange(len(my_list))
     shaker_list.append(my_list.pop(i))
 print(shaker_list)
+
+
+# разбор ДЗ-2 на семинаре 4 задача 3
+
+def create_list():
+    new_list = []
+    for i in range(10):
+        new_list.append(i)
+
+    return new_list
+
+
+my_list = create_list()
+
+
+def my_shuffle(my_list: list):
+    ni = random.randint(0, len(my_list) - 1)
+    for i in range(len(my_list)):
+        my_list[i], my_list[ni] = my_list[ni], my_list[i]
+
+
+print(my_list)
+my_shuffle(my_list)
+print(my_list)
+
+# другой вариант
+
+
+def create_list():
+    new_list = []
+    for i in range(10):
+        new_list.append(i)
+
+    return new_list
+
+
+my_list = create_list()
+
+
+def my_shuffle(my_list: list):
+    new_list = []
+    while len(my_list) > 0:
+        ni = random.randint(0, len(my_list) - 1)
+        new_list.append(my_list.pop(ni))
+
+    return new_list
+
+
+print(my_list)
+print(my_shuffle(my_list))
