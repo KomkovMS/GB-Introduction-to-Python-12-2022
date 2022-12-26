f4bebce6d9f6a0960140dcc7078beb10d90e8ec5
KomkovMS <maxkoma05@gmail.com>
2022-12-26 19:51:10 +0700

    Разбор ДЗ-2 задача 2 - Задайте список из n чисел последовательности (1 + 1/n)**n, выведеите его на экран, а так же сумму элементов списка.

--- Hw_2/HwTask_7.py
+++ Hw_2/HwTask_7.py
@@ -18,3 +18,37 @@ for i in range(len(my_list)):
     res += my_list[i]
 
 print(f'Для n = {num} -> {my_list}\nСумма {res}')
+
+
+# разбор ДЗ-2 на семинаре 4 задача 2
+number = int(input('Enter a number: '))
+
+my_list = []
+
+for n in range(1, number + 1):
+    my_list.append(round((1 + 1 / n) ** n, 2))
+
+print(f'При n = {number} список -> ', end='')
+print(*my_list, sep=', ')
+print(f'И его сумма -> {sum(my_list)}')
+
+# другой вариант решения
+number = int(input('Enter a number: '))
+
+my_list = []
+
+for n in range(1, number + 1):
+    num = round((1 + 1 / n) ** n, 2)
+    my_list.append(num if num != int(num) else int(num))
+
+print(f'При n = {number} список -> ', end='')
+print(*my_list, sep=', ')
+print(f'И его сумма -> {sum(my_list)}')
+
+
+# num if num != int(num) else int(num) тоже самое что,
+
+# if num != int(num):
+#     num = num
+# else:
+#     num = int(num)
