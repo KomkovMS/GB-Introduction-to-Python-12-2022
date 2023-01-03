8c0de09706d82f48817bc59b37a12b430549f051
KomkovMS <maxkoma05@gmail.com>
2023-01-03 13:20:58 +0700

    Решение задачи 1 discord (string)

---
+++ Other/discort_str/task1.py
@@ -0,0 +1,18 @@
+'''
+1. Сформировать строку из 10 символов. 
+На четных позициях должны находится четные цифры, на нечетных позициях - буквы.
+'''
+
+# import string
+
+# my_str = string.ascii_letters + string.digits
+# new_str = ''
+
+# print(my_str)
+
+# for i in range(10):
+#     if my_str[i].isdigit():
+#         if i % 2 == 0:
+#             new_str[i] += my_str[i]
+
+# print(new_str)
