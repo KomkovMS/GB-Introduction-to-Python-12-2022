d5e6cae2b75fb0b79ccfd2fb3a5a9dd6dc826157
KomkovMS <maxkoma05@gmail.com>
2023-01-03 22:41:14 +0700

    Решение задачи 11 на стриме дек 2022 (HwTask_11_2_.py)

--- Hw_3/HwTask_11_2_.py
+++ Hw_3/HwTask_11_2_.py
@@ -5,8 +5,11 @@
 # Пример:
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
-
 #  решение другим способом
+
+
+from random import uniform as UF
+from random import randint as RI
 
 
 def f_get_list_result(my_list: list) -> list:
@@ -39,3 +42,19 @@ result = 0
 result = f_result(list_result)
 
 print(f'{my_list} => {result}')
+
+
+# решение на стриме дек 2022
+
+list_ = [round(UF(0, 100), RI(0, 3)) for _ in range(10)]
+
+new_list = []
+for item in list_:
+    if item != int(item):
+        new_list.append(round(item % 1, 3))
+
+print(list_)
+print(new_list)
+print(
+    f'Разница между максимальной ({max(new_list)}) минимальной ({min(new_list)}'
+    f') дробными частями будет {max(new_list) - min(new_list)}')
