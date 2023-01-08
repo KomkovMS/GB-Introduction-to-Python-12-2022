11bd81dc57711256e00a666b5fb1b6741654929f
KomkovMS <maxkoma05@gmail.com>
2023-01-08 19:36:30 +0700

    Решение задачи 2 на стриме 07.01.2023 (C:\Python\Python_12.2022\Other\Stream_07jan23.py\Task2.py)

--- Other/Stream_07jan23.py/Task2.py
+++ Other/Stream_07jan23.py/Task2.py
@@ -5,20 +5,25 @@
 Дан текст. Найдите наибольшее количество идущих подряд цифр
 '''
 
+
+def get_max_digits(string: str) -> int:
+    count = 0
+    max = 0
+    for letter in string:
+        if letter.isdigit():
+            count += 1
+        else:
+            if count > max:
+                max = count
+            count = 0
+    return max
+
+
 string = 'Div213234ide the 567546line into fragmei8it6785678nts of' \
     'thresdgw456235e in a row of walking symbols. asdege768rg'  \
     'regsdrg r745gterg e4568r 9e9 rgf456747ae457568t45hhx ttwet' \
     'ertyeryt6tert a234523465gergh.'
 
-count = 0
-max = 0
 
-for letter in string:
-    if letter.isdigit():
-        count += 1
-    else:
-        if count > max:
-            max = count
-        count = 0
-
-print(max)
+max_digit = get_max_digits(string)
+print(max_digit)
