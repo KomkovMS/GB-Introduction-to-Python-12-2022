ee34268deaba593277389a58141a0e1d012a964b
KomkovMS <maxkoma05@gmail.com>
2023-02-03 22:12:39 +0700

    sem-7_phone_book(1/2)

--- Sem_7/Tel_Book/controller.py
+++ Sem_7/Tel_Book/controller.py
@@ -1,4 +1,43 @@
-# 3
+import view  # 4
+import model  # 13
+
 
 def start():
-    pass
+    choice = ''  # 21
+    while choice != 8:  # 22
+        choice = view.main_menu()  # 3
+
+        match choice:  # 5
+            case 1:  # Открыть файл
+                model.open_file()  # 14
+                view.info(1)
+            case 2:  # Сохранить файл
+                model.save_file()   # 33
+                view.info(2)
+            case 3:  # Показать все контакты
+                view.info(3)
+                pb = model.get_phone_book()  # 19
+                # или в 1 строку view.show_contacts(model.get_phone_book())
+                view.show_contacts(pb)  # 20
+            case 4:  # Создать контакт
+                view.info(4)
+                new_contact = list(view.create_new_contact())  # 23
+                model.add_new_contact(new_contact)   # 26
+            case 5:  # Удалить контакт
+                del_ = view.delete_contact()
+                model.f_del_contact(del_)
+                view.info(5)
+            case 6:  # Изменить контакт
+                view.info(6)
+                change_ = view.f_change_contact()
+                model.change_cont(change_)
+            case 7:  # Найти контакт
+                view.info(7)
+                find = view.find_contact()  # 28
+                result = model.search_contact(find)  # 30
+                view.show_contacts(result)  # 31
+            case 8:  # Выход из программы
+                view.info(8)
+                exit()
+            case _:         # рабоатет как else
+                view.input_error()
--- Sem_7/Tel_Book/view.py
+++ Sem_7/Tel_Book/view.py
@@ -1,3 +1,70 @@
 # 4 menu
 
-commands = ['']
+commands = ['Открыть файл',
+            'Сохранить файл',
+            'Показать все контакты',
+            'Создать контакт',
+            'Удалить контакт',
+            'Изменить контакт',
+            'Найти контакт',
+            'Выход из программы']
+
+
+list_msg = ['\nФайл успешно открыт\n',
+            '\nФайл успешно сохранен\n',
+            '\nСписок контактов: \n',
+            '\nВыбран пункт "Создать контакт"\n',
+            '\nКонтакт успешно удален\n',
+            '\nКонтакт успешно изменен\n',
+            '\nВыбран пункт "Найти контакт"\n',
+            '\nВыбран пункт "Выход из программы"\n']
+
+
+def main_menu() -> int:  # 5
+    print('Главное меню:')
+    for i, item in enumerate(commands, 1):
+        print(f'\t{i}. {item}')
+    choise = int(input('Выберите пункт меню: '))
+    return choise
+
+
+def info(number: int):
+    for i, item in enumerate(list_msg, 1):
+        if number == i:
+            print(item)
+
+
+def show_contacts(phone_list: list):    # 17
+    if len(phone_list) < 1:
+        print('Телефонная книга пуста или не открыта')
+    else:   # 22
+        print()
+        for i, contact in enumerate(phone_list, 1):
+            print(f'\t{i}.{contact[0]:20} {contact[1]:13} {contact[2]:20}')
+        print()
+
+
+def input_error():  # 21
+    print('Ошибка ввода. Введите корректный пункт меню')
+
+
+def create_new_contact():  # 24
+    name = input('Введите имя и фамилию: ')
+    phone = input('Введите телефон: ')
+    comment = input('Введите комментарий: ')
+    return name, phone, comment
+
+
+def find_contact():  # 27
+    find = input('Введите искомый элемент: ')
+    return find
+
+
+def delete_contact():
+    delete = input('Введите контакт для удаления: ')
+    return delete
+
+
+def f_change_contact():
+    change = input('Введите контакт для изменения: ')
+    return
