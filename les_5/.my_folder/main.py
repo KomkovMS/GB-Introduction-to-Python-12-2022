7e6fc2b53b0a157d9586e43f5f37ae641be5afb4
KomkovMS <maxkoma05@gmail.com>
2023-02-10 23:44:42 +0700

    les5 - Урок 5. Возможна ли жизнь без PIP?

---
+++ les_5/.my_folder/main.py
@@ -0,0 +1,35 @@
+# from progress.bar import Bar
+# import time
+
+# bar = Bar('Processing', max=20)
+# for i in range(20):
+#     time.sleep(1)
+#     # Do some work
+#     bar.next()
+# bar.finish()
+
+# import emoji
+
+# print(emoji.emojize('Python is :thumbs_up:'))
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# list = [1, 2, 3, 2, 7]
+# plt.plot(list)
+
+# plt.show()
+
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from bot_commands import *
+
+app = ApplicationBuilder().token(
+    "6224618510:AAGKxctKUi_ukrvQPLQpPgoiNPfBFOca_ek").build()
+
+app.add_handler(CommandHandler("hi", hi_command))
+app.add_handler(CommandHandler("time", time_command))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("sum", sum_command))
+
+app.run_polling()
